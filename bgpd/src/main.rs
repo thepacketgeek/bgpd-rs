778d3bee13ad0a189ec9534cf52d95a787b4d9ab
Mat Wood <mat@thepacketgeek.com>
2019-07-31 16:32:46 -0700

    Moving to StructOpt from Clap

--- bgpd/src/main.rs
+++ bgpd/src/main.rs
@@ -3,66 +3,33 @@
 use std::io::Result;
 use std::net::IpAddr;
 
-use clap::{App, Arg};
 use env_logger::Builder;
 use log::{debug, info, LevelFilter};
+use structopt::StructOpt;
 
 use bgpd::{serve, ServerConfig};
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "bgpd", rename_all = "kebab-case")]
+/// BGPd Server
+struct Args {
+    /// Path to bgpd server config
+    config_path: String,
+    #[structopt(short, long, default_value = "127.0.0.1")]
+    /// Path to bgpd server config
+    address: IpAddr,
+    #[structopt(short, long, default_value = "179")]
+    /// Path to bgpd server config
+    port: u16,
+    #[structopt(short, parse(from_occurrences))]
+    /// Path to bgpd server config
+    verbose: u8,
+}
+
 fn main() -> Result<()> {
-    let matches = App::new("bgpd")
-        .version("0.1")
-        .author("Mat W. <mat@thepacketgeek.com>")
-        .about("BGP Server")
-        .arg(
-            Arg::with_name("configpath")
-                .short("c")
-                .long("config-path")
-                .help("Path to bgpd server config")
-                .takes_value(true)
-                .index(1)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("address")
-                .short("a")
-                .long("address")
-                .help("IP Address to listen on")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("port")
-                .short("p")
-                .long("port")
-                .takes_value(true)
-                .help("TCP Port to listen on"),
-        )
-        .arg(
-            Arg::with_name("v")
-                .short("v")
-                .multiple(true)
-                .help("Sets the level of logging verbosity"),
-        )
-        .get_matches();
+    let args = Args::from_args();
 
-    let config_path: String = matches
-        .value_of("configpath")
-        .unwrap()
-        .parse()
-        .expect("Must specify a valid config path");
-
-    let addr: IpAddr = matches
-        .value_of("address")
-        .unwrap_or("127.0.0.1")
-        .parse()
-        .expect("Must specify a valid IP Address");
-    let port: u16 = matches
-        .value_of("port")
-        .unwrap_or("179")
-        .parse()
-        .expect("Port must be an integer");
-
-    let (bgpd_level, other_level) = match matches.occurrences_of("v") {
+    let (bgpd_level, other_level) = match args.verbose {
         0 => (LevelFilter::Info, LevelFilter::Warn),
         1 => (LevelFilter::Debug, LevelFilter::Warn),
         2 => (LevelFilter::Trace, LevelFilter::Warn),
@@ -74,10 +41,10 @@ fn main() -> Result<()> {
         .init();
     info!("Logging at levels {}/{}", bgpd_level, other_level);
 
-    let config = ServerConfig::from_file(&config_path)?;
-    debug!("Found {} peers in {}", config.peers.len(), config_path);
+    let config = ServerConfig::from_file(&args.config_path)?;
+    debug!("Found {} peers in {}", config.peers.len(), args.config_path);
 
-    serve(addr, port, config)?;
+    serve(args.address, args.port, config)?;
 
     Ok(())
 }
